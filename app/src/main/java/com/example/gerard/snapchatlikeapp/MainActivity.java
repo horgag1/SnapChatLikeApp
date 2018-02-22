12c1c211b4b6d003474987cf935bcfe68c2201a9
Gerard <gerard@Gerards-MacBook-Pro.local>
2018-02-22 16:52:19 +0000

    first commit

---
+++ app/src/main/java/com/example/gerard/snapchatlikeapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.gerard.snapchatlikeapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
